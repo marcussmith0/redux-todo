ad38d95cfc222046f2d80a977c002b9fbb8fceb9
Marcus Smith <marcussmith0711@gmail.com>
2017-09-01 10:58:11 -0500

    fixed up the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reduxForm } from 'redux-form';
 
 import todos from './index';
 
 const rootReducer = combineReducers({
-    todos
+    todos: todos,
+    form: reduxForm
 });
 
 export default rootReducer;
