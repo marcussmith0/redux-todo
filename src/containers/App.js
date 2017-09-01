3fefd9b0be2c52b4f72d75047da3491c7a9fe049
Marcus Smith <marcussmith0711@gmail.com>
2017-09-01 08:16:16 -0500

    added npm packages added SearchBar and more

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <div>
-                App
+            <div className="container">
+                <h1>Todo List App</h1>
+                
             </div>
         )
     }
