9c4356d834be57f936f5ef2e70fc51b5683555de
Marcus Smith <marcussmith0711@gmail.com>
2017-09-01 10:56:08 -0500

    fixed up the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+import todos from './index';
 
+const rootReducer = combineReducers({
+    todos
 });
 
 export default rootReducer;
