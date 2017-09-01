82c7c5da8b75b1a10f91b260702a001187f856c7
Marcus Smith <marcussmith0711@gmail.com>
2017-09-01 10:35:08 -0500

    just hooked up the input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Field } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
+
+import { createTodo } from '../actions/index';
 
 class Input extends Component {
 
-    render() {
+    renderInput() {
+
         return (
-            <form>
-                <input type="text" />
-                <button>Add</button>
+            <div>
+                <input 
+                className="form-control" 
+                type="text"
+                placeholder="Enter you to-do"
+                />
+            </div>
+        )
+    }
+    
+    onSubmit() {
+
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                name="input"
+                component={this.renderInput}
+                />
+                <button type="submit">Add</button>
             </form>
         )
     }
 }
 
 
-export default Input
+export default reduxForm ({ 
+    form: "Input"}
+)(
+    connect(null, { createTodo })(Input)
+)
