0ff66a7461d5d6191a4da8cdd5e3e1f120671f7d
Marcus Smith <marcussmith0711@gmail.com>
2017-09-01 10:53:42 -0500

    made todo reducer

---
+++ src/reducers/reducers_todo.js
@@ -0,0 +1,22 @@
+import _ from "lodash";
+
+import { CREATE_TODO } from '../actions/index';
+
+
+let id = 0;
+
+export default function (state= {}, action) {
+    switch(action.payload) {
+
+        case CREATE_TODO:
+            action.payload.id = id++;
+            const keysMapped = _.mapKeys(action.payload, "id");
+            
+            return { ...state, ...keysMapped }
+
+
+
+        default:
+            return state
+    }
+}
