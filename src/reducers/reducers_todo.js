4e71c9bdaec89756291a166034a9f1b5d8bf8b78
Marcus Smith <marcussmith0711@gmail.com>
2017-09-01 10:54:07 -0500

    made todo reducer

--- src/reducers/reducers_todo.js
+++ src/reducers/reducers_todo.js
@@ -11,10 +11,8 @@ export default function (state= {}, action) {
         case CREATE_TODO:
             action.payload.id = id++;
             const keysMapped = _.mapKeys(action.payload, "id");
-            
+
             return { ...state, ...keysMapped }
-
-
 
         default:
             return state
